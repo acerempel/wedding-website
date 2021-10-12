cc40f1b29870a1fec52e7e736c87a27c7de4af0d
Alan Rempel <alan.rempel@gmail.com>
2021-10-12 14:51:47 -0400

    Prepend underscore to unused parameters

--- resources/js/site.ts
+++ resources/js/site.ts
@@ -84,11 +84,11 @@ for (const link of navlist!.querySelectorAll('a[href]')) {
 const navShow = document.getElementById('nav-show')!
 const navHide = document.getElementById('nav-hide')!
 const navList = document.getElementById('home-nav')!
-navShow.addEventListener('click', (event) => {
+navShow.addEventListener('click', (_event) => {
     navList.classList.remove('hidden')
     navList.setAttribute('aria-expanded', 'true')
 })
-navHide.addEventListener('click', (event) => {
+navHide.addEventListener('click', (_event) => {
     navList.classList.add('hidden')
     navList.setAttribute('aria-expanded', 'false')
 })
