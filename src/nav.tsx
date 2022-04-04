bed2979a2f8ca2b867200766e9827ee8a3e6ee17
Alan Rempel <alan.rempel@gmail.com>
2022-04-04 11:45:03 -0400

    Various improvements aesthetic to mobile nav

--- src/nav.tsx
+++ src/nav.tsx
@@ -21,20 +21,20 @@ export default () => {
     <nav class="grid-left justify-self-end sm:justify-self-stretch sticky top-4 sm:top-0 sm:relative z-10">
       <button
         type="button"
-        class="block sm:hidden button drop-shadow-md"
+        class="block sm:hidden button button-teal drop-shadow-md"
         onclick={() => setOpened(true)}
       >Contents</button>
       <div
         class={`
           absolute sm:sticky top-0 sm:top-6 right-0 text-right items-end
           sm:flex flex-col drop-shadow-lg sm:drop-shadow-none bg-teal-100
-          aubergine sm:not-courgette sm:bg-transparent ${ isNotSmall() || opened() ? '' : 'hidden' }
+          rounded sm:rounded-none sm:bg-transparent ${ isNotSmall() || opened() ? '' : 'hidden' }
         `}
         id="home-nav"
         aria-expanded="false">
         <button
           type="button" aria-controls="home-nav"
-          class="block sm:hidden button mb-4 ml-auto"
+          class="block sm:hidden font-bold px-3 mr-3 py-2 mt-2 ml-auto rounded hover:bg-teal-200 active:bg-teal-200"
           onclick={() => setOpened(false)}
         >Close</button>
         <TOC />
